86becd088877c2bb502a54c7ac905db68b02fbe9
SurajPratap07 <105055042+SurajPratap07@users.noreply.github.com>
2022-05-10 02:35:16 -0700

    added print stmt

--- src/ComputerScience.java
+++ src/ComputerScience.java
@@ -2,5 +2,6 @@ public class ComputerScience {
     public static void main(String[] args) {
 
         System.out.println("yes this is github class");
+        System.out.println("i am attend a java project");
     }
 }
