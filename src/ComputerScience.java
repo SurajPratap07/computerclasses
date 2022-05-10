22572ad2754a5b90e0bffcea694228fee3026c84
SurajPratap07 <105055042+SurajPratap07@users.noreply.github.com>
2022-05-10 02:44:28 -0700

    git demo

--- src/ComputerScience.java
+++ src/ComputerScience.java
@@ -3,6 +3,7 @@ public class ComputerScience {
 
         System.out.println("yes this is github class");
         System.out.println("i am attend a java project");
+        System.out.println("yes i am attend github ");
 
     }
 }
